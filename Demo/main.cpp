ca6e4345e873d98337cea94e97fb9207023338da
Felix Morgner <felix.morgner@gmail.com>
2011-10-02 14:27:20 +0200

    updated demo app

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -16,13 +16,25 @@ int main (int argc, const char * argv[])
 	FMMP4Atom atom2(" two");
 	FMMP4Atom atom22(" two");
 	FMMP4Atom atom3("thre");
+
+	std::cout << "address of atom1: " << &atom1 << std::endl;
+	std::cout << "address of atom2: " << &atom2 << std::endl;
+	std::cout << "address of atom22: " << &atom22 << std::endl;
+	std::cout << "address of atom3: " << &atom3 << std::endl << std::endl;
 	
 	atom1.ChildAdd(&atom2);
 	atom1.ChildAdd(&atom22);
 	atom1.ChildAdd(&atom3);
 
-	FMMP4Atom *found = atom1.ChildGet(&atom2);
+	FMMP4Atom *found = atom1.ChildGet(&atom3);
 	std::vector<FMMP4Atom*> otherFound = atom1.ChildrenGet(" two");
+
+	std::cout << "found " << found << " for search for atom3"<< std::endl;
+
+	for(std::vector<FMMP4Atom*>::iterator it = otherFound.begin(); it != otherFound.end(); it++)
+		{
+		std::cout << "found " << *it << " for search for atom with name being \" two\"" << std::endl;
+		}
 
 	return 0;
 	}
